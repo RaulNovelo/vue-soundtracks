50cf97f0fba3fe9368fa5edf2e5ac14be7fb1c07
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-04-08 23:21:30 -0500

    Add base design for app

---
+++ src/components/Main.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <nav
+      class="navbar is-right is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="../assets/logos_adc.png" width="auto" height="auto" />
+        </a>
+        <a
+          class="navbar-burger burger is-active"
+          role="button"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start"></div>
+        <div class="navbar-end"></div>
+      </div>
+    </nav>
+    <section class="hero is-success is-medium">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Vue Soundtrack App</h1>
+          <h2 class="subtitle">Canciones que están Vue-nísimas!!</h2>
+        </div>
+      </div>
+    </section>
+    <Search />
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>VueSoundtrack</strong>
+          &copy; 2020 by
+          <a href="https://github.com/RaulNovelo">RaulNovelo</a>
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  components: {
+    Search: () => import('@/components/Search')
+  }
+}
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="section">
+    <div class="field has-addons">
+      <div class="control is-expanded">
+        <input
+          class="input is-large"
+          type="text"
+          placeholder="Search songs..."
+        />
+      </div>
+      <p class="control">
+        <a class="button is-large is-info">
+          <span class="icon is-large">
+            <i class="fas fa-search"></i>
+          </span>
+        </a>
+      </p>
+      <p class="control">
+        <a class="button is-large is-danger">
+          <span class="icon is-large">
+            <i class="fas fa-times"></i>
+          </span>
+        </a>
+      </p>
+    </div>
+    <div class="columns is-multiline">
+      <!-- user un v-for -->
+      <div class="column is-one-quarters">
+        <!-- Componente TrackDetail -->
+      </div>
+    </div>
+  </section>
+</template>
