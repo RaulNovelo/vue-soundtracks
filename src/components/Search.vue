ca8fcae5dbb9062565262bb80b33f4cf5b5a5c88
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-04-08 23:50:09 -0500

    Create search track handlers

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,12 +4,13 @@
       <div class="control is-expanded">
         <input
           class="input is-large"
+          v-model="searchQuery"
           type="text"
           placeholder="Search songs..."
         />
       </div>
       <p class="control">
-        <a class="button is-large is-info">
+        <a class="button is-large is-info" @click="search">
           <span class="icon is-large">
             <i class="fas fa-search"></i>
           </span>
@@ -23,11 +24,26 @@
         </a>
       </p>
     </div>
-    <div class="columns is-multiline">
-      <!-- user un v-for -->
-      <div class="column is-one-quarters">
-        <!-- Componente TrackDetail -->
-      </div>
-    </div>
   </section>
 </template>
+
+<script>
+import { searchTrack } from '@/api/tracks'
+export default {
+  data () {
+    return {
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  methods: {
+    search () {
+      searchTrack(this.searchQuery)
+        .then(res => {
+          console.log(res.data)
+          this.tracks = res.data.tracks
+        })
+    }
+  }
+}
+</script>
