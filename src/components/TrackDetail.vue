6378faded9b5b484119a60783447a22b40a327b7
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-04-09 01:10:21 -0500

    Create tracks mixin

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,50 +1,46 @@
 <template>
-  <div class="card">
-    <div class="card-image">
-      <figure class="image is-1by1">
-        <img :src="track.album.images[0].url" alt="Placeholder image" />
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
+   <div class="card">
+      <div class="card-image">
+         <figure @click="toTrack(track)" class="image is-1by1">
             <img :src="track.album.images[0].url" alt="Placeholder image" />
-          </figure>
-        </div>
-        <div class="media-content">
-          <strong
-            ><a class="title is-6">{{ track.name }}</a></strong
-          >
-          -
-          <a class="subtitle is-6">{{ track.artists[0].name }}</a>
-          <p class="subtitle is-6">{{ track.duration_ms | msToMm }}</p>
-        </div>
+         </figure>
       </div>
-    </div>
-    <footer class="card-footer">
-      <a id="play" class="card-footer-item button is-success is-inverted">
-        <span class="icon">
-          <i class="fa fa-play"></i>
-        </span>
-        <span>Preview</span>
-      </a>
-      <a
-        :href="track.external_urls.spotify"
-        target="_blank"
-        class="card-footer-item is-primary is-inverted button"
-      >
-        <span class="icon">
-          <i class="fab fa-spotify"></i>
-        </span>
-        <span>Play on Spotify</span>
-      </a>
-    </footer>
-  </div>
+      <div class="card-content">
+         <div class="media">
+            <div class="media-left">
+               <figure class="image is-48x48">
+                  <img :src="track.album.images[0].url"  alt="Placeholder image" />
+               </figure>
+            </div>
+            <div class="media-content">
+               <strong><a @click="toTrack(track)" class="title is-6">{{ track.name }}</a></strong>
+               -
+               <a class="subtitle is-6">{{ track.artists[0].name }}</a>
+               <p class="subtitle is-6">{{ track.duration_ms | msToMm }}</p>
+            </div>
+         </div>
+      </div>
+      <footer class="card-footer">
+         <a id="play" @click="selectTrack(track)" class="card-footer-item button is-success is-inverted">
+         <span class="icon">
+         <i class="fa fa-play"></i>
+         </span>
+         <span>Preview</span>
+         </a>
+         <a :href="track.external_urls.spotify" target="_blank" class="card-footer-item is-primary is-inverted button">
+         <span class="icon">
+         <i class="fab fa-spotify"></i>
+         </span>
+         <span>Go to Spotify</span>
+         </a>
+      </footer>
+   </div>
 </template>
 
 <script>
+import { trackMixin } from '@/mixins/track'
 export default {
-  props: ['track']
+  props: ['track'],
+  mixins: [trackMixin]
 }
 </script>
