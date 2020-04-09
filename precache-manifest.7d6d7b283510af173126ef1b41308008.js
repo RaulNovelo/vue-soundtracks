d56607eea508cd97bfae1e2b9e737b62d03f1793
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-04-09 00:10:08 -0500

    Publishing folder dist to gh-pages in 4/9/2020, 12:10:03 AM

---
+++ precache-manifest.7d6d7b283510af173126ef1b41308008.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072efb68e1b1090e4929",
+    "url": "/vue-soundtracks/css/app.8b7cda92.css"
+  },
+  {
+    "revision": "ea0b726c4ff0a842a812465a8aa08df3",
+    "url": "/vue-soundtracks/img/logos_adc.ea0b726c.png"
+  },
+  {
+    "revision": "256e39b8f021ce6736c0c03bcbc9bf35",
+    "url": "/vue-soundtracks/index.html"
+  },
+  {
+    "revision": "6c8b03274437c6e300b7",
+    "url": "/vue-soundtracks/js/about.fed5365d.js"
+  },
+  {
+    "revision": "072efb68e1b1090e4929",
+    "url": "/vue-soundtracks/js/app.240687ef.js"
+  },
+  {
+    "revision": "de642107a53d19fe0ab5",
+    "url": "/vue-soundtracks/js/chunk-2d207f22.4e24c07e.js"
+  },
+  {
+    "revision": "e02b1ea2cf06d9601a72",
+    "url": "/vue-soundtracks/js/chunk-4b94ef39.08dd38b9.js"
+  },
+  {
+    "revision": "72c79a10569aa18c1f87",
+    "url": "/vue-soundtracks/js/chunk-vendors.75167251.js"
+  },
+  {
+    "revision": "ae8bebe1a17296991a124b2fec56c423",
+    "url": "/vue-soundtracks/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-soundtracks/robots.txt"
+  }
+]);
