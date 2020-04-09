8104cf8802d1bd5ce5a6ee99a5f167f2568d3bb1
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-04-08 23:57:03 -0500

    Create track details component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -24,6 +24,15 @@
         </a>
       </p>
     </div>
+    <div class="columns is-multiline">
+      <div
+        v-for="track in tracks"
+        :key="track.id"
+        class="column is-one-quarters"
+      >
+        <track-detail :track="track"></track-detail>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -32,17 +41,21 @@ import { searchTrack } from '@/api/tracks'
 export default {
   data () {
     return {
-      searchQuery: '',
+      searchQuery: 'rock',
       tracks: []
     }
   },
+  // lifecycle hook
+  created () {
+    this.search()
+  },
+  components: { TrackDetail: () => import('./TrackDetail') },
   methods: {
     search () {
-      searchTrack(this.searchQuery)
-        .then(res => {
-          console.log(res.data)
-          this.tracks = res.data.tracks
-        })
+      searchTrack(this.searchQuery).then(res => {
+        console.log(res.data)
+        this.tracks = res.data.tracks.items
+      })
     }
   }
 }
